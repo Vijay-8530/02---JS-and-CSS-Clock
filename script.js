3dd2ec1b9e2ba798b2f7c8c4cdfd8e1192b740c6
vijay-8530 <vijaysangani2015@gmail.com>
2023-04-21 19:01:36 +0530

    clock with sound

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const secondHand = document.querySelector('.second-hand')
 const minHand = document.querySelector('.min-hand')
 const hourhand = document.querySelector('.hour-hand')
+const audio = document.querySelector('.audio')
+audio.loop = true
+console.log(audio.loop)
+// console.log(audio)
 // console.log(secondHand,minHand,hourhand)
 const setDate = () => {
     // console.log('hello','hi')
@@ -12,10 +16,12 @@ const setDate = () => {
 
     const minutes = now.getMinutes()
     const minitsDegrees = ((minutes / 60) * 360 + 88)
-    minHand.style.transform = `rotate(${minitsDegrees}deg)`
+    minHand.style.transform = `rotate(${minitsDegrees}deg)`;
+    // audio.play()
+
 
     const hourse = now.getHours()
-    const hourseDegree = ((hourse / 12) * 360 + 85)
+    const hourseDegree = ((hourse / 12) * 360 + 95)
     hourhand.style.transform = `rotate(${hourseDegree}deg)`
     // console.log(hourse,minutes,second)
 }
